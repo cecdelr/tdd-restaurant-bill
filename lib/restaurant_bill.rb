0a69a5bda368e573d45ae2d44c0b9225ea2153e2
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2017-02-24 12:29:51 -0800

    add restaurant_bill starter code

---
+++ lib/restaurant_bill.rb
@@ -0,0 +1,12 @@
+# class RestaurantBill
+#   attr_reader :ordered_items
+
+#   def initialize
+#     @ordered_items = []
+#   end
+
+#   def order_item(item, cost)
+#     @ordered_items << [item, cost]
+#   end
+
+# end
